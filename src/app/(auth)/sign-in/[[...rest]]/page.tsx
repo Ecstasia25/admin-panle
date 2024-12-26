b4b4213c0e9ce7bf3fd2635f206547b657cad0ba
Suman Mondal <mondalsumanrock20@gmail.com>
2024-12-26 22:30:12 +0530

    feat: remove welcome page and update sign-in/sign-up components; add Clerk webhook for user creation

--- src/app/(auth)/sign-in/[[...rest]]/page.tsx
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -5,13 +5,10 @@ import { useSearchParams } from "next/navigation"
 
 const Page = () => {
   const searchParams = useSearchParams()
-  const intent = searchParams.get("intent")
 
   return (
     <div className="w-full flex-1 flex items-center justify-center">
       <SignIn
-        forceRedirectUrl={"/welcome"}
-        fallbackRedirectUrl={"/welcome"}
       />
     </div>
   )
