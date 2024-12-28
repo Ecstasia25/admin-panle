07977fe4789aeec62b3105e96a0381073dac0c89
Suman Mondal <mondalsumanrock20@gmail.com>
2024-12-28 18:17:55 +0530

    feat: integrate PWA support with next-pwa and update manifest for improved caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+import nextPWA from "@ducanh2912/next-pwa"
+
+const withPWA = nextPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+})
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -14,4 +28,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPWA(nextConfig)
