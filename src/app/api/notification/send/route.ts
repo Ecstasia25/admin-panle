169a3d7be78d9aa2c9402cab36301dde5dfb1e9f
Suman Mondal <mondalsumanrock20@gmail.com>
2025-01-03 22:39:41 +0530

    feat: remove notification sending functionality and add new service worker for caching

--- src/app/api/notification/send/route.ts
+++
@@ -1,6 +0,0 @@
-import sendFirebaseNotification from "@/controller/firebase-controller"
-
-export async function POST(req: any, res: Response) {
-  const result = await sendFirebaseNotification(req, res)
-  return result
-}
